6f2288702310ceae557303f2e51ed2efbfbd4871
Jose Carlos Rivera Hernandez <josecarlosrh03@gmail.com>
2018-04-25 00:14:02 -0300

    creamos el select de empleados en el crud

--- php/CRUD.php
+++ php/CRUD.php
@@ -6,22 +6,28 @@ class CRUD
 {
    private $conexion; 
    private $datos;
-   
+   private $resultado;
 
    public function __construct(){ }
 
-   public SelectEmpleado($contrasena, $nombre)
+   public function SelectEmpleado($contrasena, $nombre)
    {
       $this->conexion = dbc::GetConexion();
-      this->tados = $this->conexion->query("SELECT nombre,tipo from empleados where usuario = ".$nombre." and contrasena = ".$contrasena);
+      $this->datos = $this->conexion->prepare('SELECT * FROM empleados WHERE nombre = :nombre AND contrasena = :contrasena');
+      $this->datos->execute(array(':nombre'=>$nombre , 'contrasena'=> $contrasena));
       
-      echo $this->datos;
-     
-      dbc::GetDesconectar();
+      if ($this->datos != null){
+       $this->resultado = $this->datos->fetch();
+       print_r($this->resultado);
+      }else{
+        echo("error");
+      }
+
+     // dbc::GetDesconectar();
    }
-   
-  $crud = new CRUD();
-  $crud->SelectEmpleado("12341234","Jose Carlos");
 }
 
+$crud = new CRUD();
+$crud->SelectEmpleado("12341234","Jose Carlos");
 ?>
+
