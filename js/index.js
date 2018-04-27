bb802222aa9f4ae94482ce282b7db421a990b4e8
Jose Carlos Rivera Hernandez <josecarlosrh03@gmail.com>
2018-04-27 16:29:49 -0400

    .html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$('#boton').on('click','#formulario', function(){
+$(document).on('click','#formulario', function(){
 
    // e.preventDefault();
     
@@ -8,13 +8,13 @@ $('#boton').on('click','#formulario', function(){
     .done(function(user){
         if(user[2] == 'admin'){
             $('#nombre').val('');
-            location.href = '../paginas/admin.php';
+            location.href = '../paginas/admin.html';
         }else if(user[2] == 'rango2'){
             $('#nombre').val('');
-            location.href = '../paginas/registro.php';
+            location.href = '../paginas/registro.html';
         }else if(user[2] == 'rango1'){
             $('#nombre').val('');
-            location.href = '../paginas/escaner.php';
+            location.href = '../paginas/escaner.html';
         }else{
            $('#formulario p').html('<p>confirmar que todo está correcto</p>');
            $('#formulario p').css({color:'red'});
