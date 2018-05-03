87c4980e847b1197ce7178b58baadec7b6c7da86
Jose Carlos Rivera Hernandez <josecarlosrh03@gmail.com>
2018-05-03 09:22:10 -0400

    solo falta el mandar los datos por php a la db y el ajax y la validaciones con el  metodo input

---
+++ js/registro.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    var control = 0;
+$('#boton1').on('click',function(){
+  if(control >0){
+    $('.boton').attr("disabled", true);
+  }else{
+    $('.boton').attr("disabled", false);
+    $('#persona').hide();
+    $('#vehiculo').fadeIn(1000);
+    $('#a2').css({
+        "color":"black",
+        "background":"white" 
+    })
+    $( "<style> #a2:before { border-left:10px solid  white; }</style>" ).appendTo( "#a2" );
+    valor = 0;
+  }
+});
+
+$('#boton2').on('click',function(){
+    $('#vehiculo').hide();
+    $('#persona').fadeIn(1000);
+    $('#a2').css({
+        "color":"white",
+        "background":"#0F218D" 
+    })
+    $( "<style> #a2:before { border-left:10px solid  #0F218D; }</style>" ).appendTo( "#a2" );
+
+});
+
+$('#boton3').on('click',function(){
+    if(control >0){
+        $('.boton').attr("disabled", true);
+      }else{
+    $('.boton').attr("disabled", false);
+    $('#vehiculo').hide();
+    $('#formulario').hide();
+    $('#datos').fadeIn(1000);
+    $('#a3').css({
+        "color":"black",
+        "background":"white" 
+    })
+    $( "<style> #a3:before { border-left:10px solid  white; }</style>" ).appendTo( "#a3" );
+    var datosFormulario = $('#formulario');
+    $('#nombre_datos').append(" "+datosFormulario[0][0].value);
+    $('#apellido_datos').append(" "+datosFormulario[0][1].value);
+    $('#cedula_datos').append(" "+datosFormulario[0][2].value);
+    $('#matricula_datos').append(" "+datosFormulario[0][4].value);
+    $('#placa_datos').append(" "+datosFormulario[0][5].value);
+    $('#marca_datos').append(" "+datosFormulario[0][6].value);
+    $('#modelo_datos').append(" "+datosFormulario[0][7].value);
+    $('#color_datos').append(" "+datosFormulario[0][8].value);
+}
+});
+
+$('#boton4').on('click',function(){
+    $('#datos').hide();
+    $('#formulario').fadeIn(1000);
+    $('#vehiculo').fadeIn(1000);
+    $('#a3').css({
+        "color":"white",
+        "background":"#0F218D" 
+    })
+    $( "<style> #a3:before { border-left:10px solid  #0F218D; }</style>" ).appendTo( "#a3" );
+});
+
+$('#boton5').on('click',function(){
+  
+});
+
+$('.validar').on('focusout',function(){
+ var valor = $(this).val();
+    if(valor == '' || valor == null){
+        control += 1 ;
+        $(this).css({"border-color":"red"});
+      }else{
+        control -=1;
+        $(this).css({"border-color":""});
+        $('.boton').attr("disabled", false);
+      }
+       
+
+})
+  
+});
--- paginas/Registro/index.php
+++ paginas/Registro/index.php
@@ -27,25 +27,25 @@ if(isset($_SESSION['usuario'])){
      <header>
          <img src="../../img/logo-uasd_cambio4.jpg" alt="" height="100px"/>
         <nav>
-            <a href="#">Datos Personales</a>
-            <a href="#">Datos del Vehiculo</a>
-            <a href="#">Perfil Completo</a>
+            <a href="#" id="a1">Datos Personales</a>
+            <a href="#" id="a2">Datos del Vehiculo</a>
+            <a href="#"id="a3" >Perfil Completo</a>
         </nav>
      </header>
      <div id="cuerpo"> 
-     <form action="">
+     <form action="" id="formulario">
      <div id="persona">
      <div id="cuerpo1">
         <h3>Datos de la Persona</h3>
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre">
-        <label for="apellido">Apellido:</label>
-        <input type="text" id="apellido">
+        <input type="text" id="nombre" class="validar">
+        <label for="apellido" >Apellido:</label>
+        <input type="text" id="apellido" class="validar">
         <label for="cedula">Cedula:</label>
-        <input type="text" id="cedula">
+        <input type="text" id="cedula" class="validar">
       </div>
       <div id="cuerpo2">
-        <input type="button" value="Siguiente" id="boton1">
+        <input type="button" value="Siguiente" id="boton1" class="boton" >
       </div>
      </div>
     <div id="vehiculo">
@@ -53,33 +53,58 @@ if(isset($_SESSION['usuario'])){
         <div id="bloque1">
             <h3>Vehiculo</h3>
             <label for="matricula">Matricula:</label>
-            <input type="text" id="matricula">
+            <input type="text" id="matricula" class="validar">
             <label for="placa">Placa:</label>
-            <input type="text" id="placa">
+            <input type="text" id="placa" class="validar">
             <label for="modelo">Marca:</label>
-            <input type="text" id="marca">
+            <input type="text" id="marca" class="validar">
         </div>
         
         <div id="bloque2">
             <label for="modelo">Modelo:</label>
-            <input type="text" id="modelo">
+            <input type="text" id="modelo" class="validar">
             <label for="modelo">Color:</label>
-            <input type="text" id="color">
+            <input type="text" id="color" class="validar">
         </div>
         
       </div>
       <div id="cuerpo4">
         <input type="button" value="Atras" id="boton2">
-        <input type="button" value="Siguiente" id="boton3">
+        <input type="button" value="Siguiente" id="boton3" class="boton" >
       </div>
     </div>
      </form>
+     <div id="datos">
+
+      <img src="../../img/2.png" id="foto">
+     <div id="datos_p">
+        <div  id="bloque1_datos">
+          <p id="nombre_datos"><strong>Nombre:</strong> </p>
+          <p id="apellido_datos"><strong>Apellido:</strong> </p>
+          <br><br>
+          <p id="cedula_datos"><strong>Cedula:</strong> </p>
+        </div>
+         <div id="bloque2_datos">
+         <p id="matricula_datos"><strong>Matricula:</strong> </p>
+         <p id="placa_datos"><strong>Placa:</strong> </p>
+         <p id="marca_datos"><strong>Marca:</strong> </p>
+         <br><br>
+         <p id="modelo_datos"><strong>Modelo:</strong> </p>
+         <p id="color_datos"><strong>Color:</strong> </p>
+         </div>
+     </div>
+     <div id="botones_datos">
+      <input type="button" value="Atras" id="boton4">
+      <input type="button" value="Confirmar" id="boton5" class="boton" >
+     </div>
+     </div>
      </div>
      <footer>
         <p>Universidad Autónoma de Santo Domingo (UASD)</p>
      </footer>
     </section>
     
-<script src="../../js/jquery-min.js"></script>       
+<script src="../../js/jquery-min.js"></script>  
+<script src="../../js/registro.js"></script>       
 </body>
 </html>
